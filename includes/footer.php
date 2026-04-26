92491da905d2b5a45439122ed7783d76c219dd6f
Farida-Amir <faridaamirmou@gmail.com>
2026-04-27 00:21:21 +0300

    uu

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,9 @@
 <footer class="footer">
   <p>© 2026 Findex Platform</p>
 </footer>
+
+</div> <!-- END page-wrapper -->
+
 <script src="/Findex/js/home.js?v=2"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -8,37 +8,31 @@ if (session_status() === PHP_SESSION_NONE) {
 <html>
 <head>
   <title>Findex</title>
-<link rel="stylesheet" href="/Findex/css/style.css?v=2">
+  <link rel="stylesheet" href="/Findex/css/style.css?v=2">
 </head>
 
 <body>
+<div class="page-wrapper">
 
 <nav class="navbar">
-
   <a href="/Findex/index.php" class="logo">Findex</a>
 
   <div class="nav-links">
-
     <a href="/Findex/index.php">Home</a>
 
     <?php if(isset($_SESSION['user_id'])): ?>
-
       <a href="/Findex/chat.php">Chat</a>
       <a href="/Findex/dashboard.php">Dashboard</a>
       <a href="/Findex/report.php">Reports</a>
       <a href="/Findex/search.php">Search</a>
 
       <span>Hi, <?= htmlspecialchars($_SESSION['name']); ?></span>
-
       <a href="/Findex/logout.php">Logout</a>
-
     <?php else: ?>
-
       <a href="/Findex/auth.php">Login</a>
       <a href="/Findex/auth.php">Register</a>
-
     <?php endif; ?>
-
   </div>
-
 </nav>
+
+<main>
